34f7827a4af9c5c146624c640e6044b86e2540c4
Evilson Souza <evilsons@yahoo.com>
2020-10-05 23:16:09 -0300

    comsumo de api e segurança

--- blogPessoal/src/app/app.module.ts
+++ blogPessoal/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -9,6 +10,9 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FeedComponent } from './feed/feed.component';
 import { PerfilLateralComponent } from './perfil-lateral/perfil-lateral.component';
+import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
@@ -17,12 +21,16 @@ import { PerfilLateralComponent } from './perfil-lateral/perfil-lateral.componen
     FooterComponent,
     NavbarComponent,
     FeedComponent,
-    PerfilLateralComponent
+    PerfilLateralComponent,
+    LoginComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ blogPessoal/src/app/model/Tema.ts
@@ -0,0 +1,8 @@
+import { from } from 'rxjs'
+import { Postagem } from "./Postagem"
+
+export class Tema {
+    public id: number
+    public descricao: string
+    public postagem: Postagem[]
+}
